a17255dc16ab609d2b8479f941991ef1b39f97f4
Philalex1964 <philalex1964@me.com>
2024-02-21 18:22:01 +0100

    set up

---
+++ CurrencyConverter/CurrencyConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConverter
+//
+//  Created by Alexander Filippov on 21.2.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
