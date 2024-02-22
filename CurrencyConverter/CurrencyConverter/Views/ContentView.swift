7913f6309d442df4c0a26c287b38df3cf03607fb
Philalex1964 <philalex1964@me.com>
2024-02-22 20:00:54 +0100

    Moved all @State properties from contentView to viewModel.

--- CurrencyConverter/CurrencyConverter/Views/ContentView.swift
+++ CurrencyConverter/CurrencyConverter/Views/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sourceAmount = ""
-    @State private var selectedSourceCurrency = Currency.usd
-    @State private var selectedTargetCurrency = Currency.rub
-    @State private var currencyRates: [String: Double] = [:]
-    @State private var conversionHistory: [ConversionEntry] = []
-    @State private var timer: Timer? 
+//    @State private var sourceAmount = ""
+//    @State private var selectedSourceCurrency = Currency.usd
+//    @State private var selectedTargetCurrency = Currency.rub
+//    @State private var currencyRates: [String: Double] = [:]
+//    @State private var conversionHistory: [ConversionEntry] = []
+//    @State private var timer: Timer? 
     
-    //    @State private var viewModel = ViewModel()
+    @State private var viewModel = ViewModel()
     
     @FocusState private var fieldIsFocused: Bool
     
     private let currencies: [Currency] = [.rub, .usd, .eur, .gbp, .chf, .cny]
     
     var convertedAmount: Double {
-        guard let sourceRate = currencyRates[selectedSourceCurrency.rawValue],
-              let targetRate = currencyRates[selectedTargetCurrency.rawValue],
-              let amount = Double(sourceAmount) else {
+        guard let sourceRate = viewModel.currencyRates[viewModel.selectedSourceCurrency.rawValue],
+              let targetRate = viewModel.currencyRates[viewModel.selectedTargetCurrency.rawValue],
+              let amount = Double(viewModel.sourceAmount) else {
             return 0.0
         }
         
@@ -35,24 +35,24 @@ struct ContentView: View {
         NavigationStack {
             Form {
                 Section(header: Text("Source")) {
-                    Picker("Source Currency", selection: $selectedSourceCurrency) {
+                    Picker("Source Currency", selection: $viewModel.selectedSourceCurrency) {
                         ForEach(currencies, id:\.self) { currency in
                             Text(currency.rawValue)
                         }
                     }
                     .pickerStyle(MenuPickerStyle())
-                    TextField("Amount", text: $sourceAmount).focused($fieldIsFocused)
+                    TextField("Amount", text: $viewModel.sourceAmount).focused($fieldIsFocused)
                         .keyboardType(.decimalPad)
                 }
                 
                 Section(header: Text("Target")) {
-                    Picker("Target Currency", selection: $selectedTargetCurrency) {
+                    Picker("Target Currency", selection: $viewModel.selectedTargetCurrency) {
                         ForEach(currencies, id: \.self) { currency in
                             Text(currency.rawValue)
                         }
                     }
                     .pickerStyle(MenuPickerStyle())
-                    Text("Converted Amount: \(convertedAmount, specifier: "%.2f") \(selectedTargetCurrency.rawValue)")
+                    Text("Converted Amount: \(convertedAmount, specifier: "%.2f") \(viewModel.selectedTargetCurrency.rawValue)")
                 }
                 
                 Button("Add to History", action: addToHistory)
@@ -64,26 +64,26 @@ struct ContentView: View {
                 loadSelectedCurrencyPair()
                 startRefreshTimer()
             }
-            .onChange(of: selectedTargetCurrency) { _, _ in
+            .onChange(of: viewModel.selectedTargetCurrency) { _, _ in
                 saveSelectedCurrencyPairLocally()
             }
-            .onChange(of: selectedTargetCurrency) { _, _ in
+            .onChange(of: viewModel.selectedTargetCurrency) { _, _ in
                 saveSelectedCurrencyPairLocally()
             }
-            .onChange(of: conversionHistory) { _, _ in
+            .onChange(of: viewModel.conversionHistory) { _, _ in
                 saveConversionHistoryLocally()
             }
             .navigationBarItems(trailing:
-                                    NavigationLink(destination: ConversionHistoryView(conversionHistory: $conversionHistory)) {
+                                    NavigationLink(destination: ConversionHistoryView(conversionHistory: $viewModel.conversionHistory)) {
                 Text("History")
             })
         }
     }
     
     func addToHistory() {
-        let entry = ConversionEntry(sourceCurrency: selectedSourceCurrency, targetCurrency: selectedTargetCurrency, sourceAmount: Double(sourceAmount) ?? 0, convertedAmount: convertedAmount, timestamp: Date())
+        let entry = ConversionEntry(sourceCurrency: viewModel.selectedSourceCurrency, targetCurrency: viewModel.selectedTargetCurrency, sourceAmount: Double(viewModel.sourceAmount) ?? 0, convertedAmount: convertedAmount, timestamp: Date())
         
-        conversionHistory.insert(entry, at: 0)
+        viewModel.conversionHistory.insert(entry, at: 0)
         fieldIsFocused = false
     }
     
@@ -92,7 +92,7 @@ struct ContentView: View {
             switch result {
             case .success(let rates):
                 DispatchQueue.main.async {
-                    self.currencyRates = rates
+                    viewModel.currencyRates = rates
                     saveCurrencyRatesLocally()
                 }
             case .failure(let error):
@@ -102,7 +102,7 @@ struct ContentView: View {
     }
     
     func saveCurrencyRatesLocally() {
-        UserDefaults.standard.set(currencyRates, forKey: "currencyRates")
+        UserDefaults.standard.set(viewModel.currencyRates, forKey: "currencyRates")
         UserDefaults.standard.set(Date(), forKey: "cachedRatesTimestamp")
     }
     
@@ -111,20 +111,20 @@ struct ContentView: View {
            let sourceCurrency = Currency(rawValue: sourceCurrencyRawValue),
            let targetCurrencyRawValue = UserDefaults.standard.string(forKey: "targetCurrency"),
            let targetCurrency = Currency(rawValue: targetCurrencyRawValue) {
-            selectedSourceCurrency = sourceCurrency
-            selectedTargetCurrency = targetCurrency
+            viewModel.selectedSourceCurrency = sourceCurrency
+            viewModel.selectedTargetCurrency = targetCurrency
         }
     }
     
     func saveSelectedCurrencyPairLocally() {
-        UserDefaults.standard.set(selectedSourceCurrency.rawValue, forKey: "sourceCurrency")
-        UserDefaults.standard.set(selectedTargetCurrency.rawValue, forKey: "targetCurrency")
+        UserDefaults.standard.set(viewModel.selectedSourceCurrency.rawValue, forKey: "sourceCurrency")
+        UserDefaults.standard.set(viewModel.selectedTargetCurrency.rawValue, forKey: "targetCurrency")
     }
     
 //MARK: - PRIVATE
     
     private func startRefreshTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 21600, repeats: true) { _ in
+        viewModel.timer = Timer.scheduledTimer(withTimeInterval: 21600, repeats: true) { _ in
             fetchRates()
         }
     }
@@ -136,7 +136,7 @@ struct ContentView: View {
     
     private func loadCachedCurrencyRates() {
         if let cachedRates = UserDefaults.standard.dictionary(forKey: "currencyRates") as? [String: Double] {
-            self.currencyRates = cachedRates
+            viewModel.currencyRates = cachedRates
         } else {
             fetchRates()
         }
@@ -144,7 +144,7 @@ struct ContentView: View {
     
     private func saveConversionHistoryLocally() {
         let encoder = JSONEncoder()
-        if let encodedHistory = try? encoder.encode(conversionHistory) {
+        if let encodedHistory = try? encoder.encode(viewModel.conversionHistory) {
             UserDefaults.standard.set(encodedHistory, forKey: "conversionHistory")
         }
     }
@@ -153,7 +153,7 @@ struct ContentView: View {
         if let savedHistoryData = UserDefaults.standard.data(forKey: "conversionHistory") {
             let decoder = JSONDecoder()
             if let loadedHistory = try? decoder.decode([ConversionEntry].self, from: savedHistoryData) {
-                self.conversionHistory = loadedHistory
+                viewModel.conversionHistory = loadedHistory
             }
         }
     }
