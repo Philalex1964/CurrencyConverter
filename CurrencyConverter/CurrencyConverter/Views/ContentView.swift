d5e28fd66184febef03bb2e9674fc6ace01cb3e0
Philalex1964 <philalex1964@me.com>
2024-02-22 20:38:50 +0100

    Cleaned project.

--- CurrencyConverter/CurrencyConverter/Views/ContentView.swift
+++ CurrencyConverter/CurrencyConverter/Views/ContentView.swift
@@ -8,18 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var sourceAmount = ""
-//    @State private var selectedSourceCurrency = Currency.usd
-//    @State private var selectedTargetCurrency = Currency.rub
-//    @State private var currencyRates: [String: Double] = [:]
-//    @State private var conversionHistory: [ConversionEntry] = []
-//    @State private var timer: Timer? 
-    
     @State private var viewModel = ViewModel()
     
     @FocusState private var fieldIsFocused: Bool
-    
-    //private let currencies: [Currency] = [.rub, .usd, .eur, .gbp, .chf, .cny]
     
     var convertedAmount: Double {
         guard let sourceRate = viewModel.currencyRates[viewModel.selectedSourceCurrency.rawValue],
@@ -86,77 +77,6 @@ struct ContentView: View {
         viewModel.conversionHistory.insert(entry, at: 0)
         fieldIsFocused = false
     }
-    
-//    func fetchRates() {
-//        CurrencyService.shared.fetchCurrencyRates { result in
-//            switch result {
-//            case .success(let rates):
-//                DispatchQueue.main.async {
-//                    viewModel.currencyRates = rates
-//                    saveCurrencyRatesLocally()
-//                }
-//            case .failure(let error):
-//                print("Failed to fetch currency rates: \(error)")
-//            }
-//        }
-//    }
-    
-//    func saveCurrencyRatesLocally() {
-//        UserDefaults.standard.set(viewModel.currencyRates, forKey: "currencyRates")
-//        UserDefaults.standard.set(Date(), forKey: "cachedRatesTimestamp")
-//    }
-    
-//    func loadSelectedCurrencyPair() {
-//        if let sourceCurrencyRawValue = UserDefaults.standard.string(forKey: "sourceCurrency"),
-//           let sourceCurrency = Currency(rawValue: sourceCurrencyRawValue),
-//           let targetCurrencyRawValue = UserDefaults.standard.string(forKey: "targetCurrency"),
-//           let targetCurrency = Currency(rawValue: targetCurrencyRawValue) {
-//            viewModel.selectedSourceCurrency = sourceCurrency
-//            viewModel.selectedTargetCurrency = targetCurrency
-//        }
-//    }
-    
-//    func saveSelectedCurrencyPairLocally() {
-//        UserDefaults.standard.set(viewModel.selectedSourceCurrency.rawValue, forKey: "sourceCurrency")
-//        UserDefaults.standard.set(viewModel.selectedTargetCurrency.rawValue, forKey: "targetCurrency")
-//    }
-    
-//MARK: - PRIVATE
-    
-//    private func startRefreshTimer() {
-//        viewModel.timer = Timer.scheduledTimer(withTimeInterval: 21600, repeats: true) { _ in
-//            viewModel.fetchRates()
-//        }
-//    }
-    
-//    private func stopRefreshTimer() {
-//        timer?.invalidate()
-//        timer = nil
-//    }
-    
-//    private func loadCachedCurrencyRates() {
-//        if let cachedRates = UserDefaults.standard.dictionary(forKey: "currencyRates") as? [String: Double] {
-//            viewModel.currencyRates = cachedRates
-//        } else {
-//            viewModel.fetchRates()
-//        }
-//    }
-    
-//    private func saveConversionHistoryLocally() {
-//        let encoder = JSONEncoder()
-//        if let encodedHistory = try? encoder.encode(viewModel.conversionHistory) {
-//            UserDefaults.standard.set(encodedHistory, forKey: "conversionHistory")
-//        }
-//    }
-    
-//    private func loadConversionHistory() {
-//        if let savedHistoryData = UserDefaults.standard.data(forKey: "conversionHistory") {
-//            let decoder = JSONDecoder()
-//            if let loadedHistory = try? decoder.decode([ConversionEntry].self, from: savedHistoryData) {
-//                viewModel.conversionHistory = loadedHistory
-//            }
-//        }
-//    }
 }
 
 #Preview {
